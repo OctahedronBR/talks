201f61202d2de8fd63b26b26efe0b94cc7dabae5
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-07-25 23:10:06 -0300

    added google analytics tracker to last talk

--- v-ensol/scripts/script.js
+++ v-ensol/scripts/script.js
@@ -206,4 +206,8 @@
 		}
 	}, false);
 
+	var _gaq=[["_setAccount","UA-22233247-1"],["_trackPageview"]];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+	g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+	s.parentNode.insertBefore(g,s)}(document,"script"));
 }());
